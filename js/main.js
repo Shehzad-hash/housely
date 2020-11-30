7e6ed11d5d2867636d1283eefa1f8c0cdb22d379
Shehzad <shehzadkhan1564008@gmail.com>
2020-11-29 19:04:59 -0800

    update

--- js/main.js
+++ js/main.js
@@ -2,23 +2,50 @@
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 12,
-    nav: false,
-    dots: false,
+    nav: true,
+    dots: true,
+    autoPlay: true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
     responsiveClass: true,
     responsive: {
         0: {
             items: 1,
-            nav: false,
-            loop: true
+            dots: true,
+            nav: true,
+            loop: true,
         },
         600: {
             items: 2,
-            nav: false
+            dots: true,
+            nav: true,
+            loop: true
         },
         1000: {
             items: 3.5,
-            nav: false,
-            loop: false
+            dots: false,
+            nav: true,
+            loop: true,
         }
+    }
+});
+
+// 2
+$('#owl-carousel').owlCarousel({
+    loop: true,
+    margin: 12,
+    nav: true,
+    dots: true,
+    autoPlay: true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            dots: true,
+            nav: true,
+            loop: true,
+        },
     }
 });
